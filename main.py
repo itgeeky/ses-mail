a1cff035105c8a1eea43e3430bc3545bd1c0485f
Carlos Soto <carlossoto@MacBook-Air-de-Carlos.local>
2024-03-30 21:33:17 -0600

    test cors

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from starlette.responses import RedirectResponse
 import boto3
 from botocore.exceptions import ClientError
 from fastapi import  Request, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://itgeeky.github.io/portfolio-eng/",
+    "https://itgeeky.github.io",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
